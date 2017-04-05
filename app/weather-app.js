2ba49e7fbec710ff20976fa700212745d781d9c3
Benjamin Roth <benni.roth91@gmail.com>
2017-04-05 17:20:03 +0200

    refactor(weather-add.js): move from HTML to JS file

--- app/weather-app.js
+++ app/weather-app.js
@@ -1,17 +1,50 @@
-/**
- * Created by benni on 28.03.17.
- */
+var weatherApp = (function () {
+        this.fetchData = function (zipCode) {
+            let jsonurl = "http://api.openweathermap.org/data/2.5/forecast?zip=" + zipCode + ",de&APPID=c314e4b540eac4e88cee00e53b7bf99f";
+            window.jQuery.get(jsonurl).then(
+                (data) => {
+                    console.log(data.city.name);
+                    if (data.city.name)
+                        window.$('#heading').text("Wetter in " + data.city.name);
+                    createChart(data);
+                },
+                (error) => {
+                    console.error(error);
+                }
+            );
+        };
 
-class WeatherApp {
-    constructor() {
-        this.data = [];
-        this.http = re
+        this.createChart = function (weatherData) {
+            let tempList = weatherData.list.map((item) => { // Return List with temperatures in Celsius.
+                let temp = item.main.temp - 273.15; // Convert Kelvin to Celsius.
+                return temp.toFixed(0);
+            });
 
-    }
+            let timeLabels = weatherData.list.map(
+                (item, index) => {
+                    let label = '';
 
-    fetch(zipCode, countryCode) {
+                    //if (index % 3 == 0)
+                    label = moment.unix(item.dt).calendar();
 
-    }
-}
+                    return label;
+                }
+            );
 
-export default WeatherApp
+            let tempCanvas = window.$('#tempCanvas');
+            let tempChart = new Chart(tempCanvas, {
+                type: 'line',
+                data: {
+                    labels: timeLabels,
+                    datasets: [
+                        {
+                            label: 'Temperatur',
+                            data: tempList,
+                            backgroundColor: "rgba(255, 153, 0, 0.6)"
+                        }
+                    ]
+                }
+            });
+        };
+        return this;
+    })();
